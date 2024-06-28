cc574a4707b5b5db3b1f7c19c2b8acb849b9b113
nagachandu2003 <nagachanduthatha@gmail.com>
2024-06-28 20:57:06 +0530

    added stats design

--- src/components/BottomTabs/Stats/index.js
+++ src/components/BottomTabs/Stats/index.js
@@ -64,11 +64,13 @@ import { Link } from "react-router-dom"
         <div style={{marginBottom:'50px',marginTop:'30px'}} className="scrollable-container">
             <div className="d2d-container">
             {isLoading===false && (
+                <>
                 <div style={{margin:'10px'}} className={`photos-tab-content ${activeTab === 0 ? 'active' : ''}`}>
                   <div className="stats-section-container">
                     <div>
+                    
+                    <div style={{height:'100px'}} className="avg-cards2">
                     <h3 className="stats-section-heading2">Attendance</h3>
-                    <div className="avg-cards2">
                         <h4>{statsdetails.attendancedetails}</h4>
                         {/* <h4>Morning : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
                         <h4>Evening : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
@@ -101,8 +103,9 @@ import { Link } from "react-router-dom"
                     </div>
                     <div className="stats-section-container">
                     <div>
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg2">
                     <h3 className="stats-section-heading2">Attendance (Selfie)</h3>
-                    <div className="avg-cards2 gr-bg2">
                         <h4>Morning Selfie : {(statsdetails.morningattendanceselfiedetails)}</h4>
                         <h4>Evening Selfie : {(statsdetails.eveningattendanceselfiedetails)}</h4>
                     </div>
@@ -124,8 +127,9 @@ import { Link } from "react-router-dom"
                     </div>
                     <div className="stats-section-container">
                     <div>
-                    <h3 className="stats-section-heading2">Household (Selfie)</h3>
-                    <div className="avg-cards2 gr-bg3">
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg3">
+                    <h3 style={{color:'black'}} className="stats-section-heading2">Household (Selfie)</h3>
                         <h4>Total Selfie : {statsdetails.householdselfiedetails}</h4>
                     </div>
                     </div>
@@ -146,8 +150,9 @@ import { Link } from "react-router-dom"
                     </div>
                     <div className="stats-section-container">
                     <div>
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg4">
                     <h3 className="stats-section-heading2">Sansthapak Sadasya</h3>
-                    <div className="avg-cards2 gr-bg4">
                         <h4>Total SS Reported  : {statsdetails.ssdetails}</h4>
                         {/* <h4>No of Blocks : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
                         <h4>No of Villages : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
@@ -157,8 +162,9 @@ import { Link } from "react-router-dom"
                     </div>
                     <div className="stats-section-container">
                     <div>
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg1">
                     <h3 className="stats-section-heading2">Digital Influencer</h3>
-                    <div className="avg-cards2 gr-bg1">
                         <h4>Total Digital Influencers Reported : {statsdetails.didetails}</h4>
                         {/* <h4>Morning Selfie : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
                         <h4>Evening Selfie : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
@@ -168,8 +174,9 @@ import { Link } from "react-router-dom"
                     <div className="stats-section-container">
                     <div className="stats-section-container">
                     <div>
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg2">
                     <h3 className="stats-section-heading2">Coaching</h3>
-                    <div className="avg-cards2 gr-bg2">
                     <h4>Total Coaching Reported : {statsdetails.coachingdetails}</h4>
                     </div>
                     </div>
@@ -177,15 +184,164 @@ import { Link } from "react-router-dom"
                     </div>
                     <div className="stats-section-container">
                     <div>
-                    <h3 className="stats-section-heading2">SS Vitran</h3>
-                    <div className="avg-cards2 gr-bg3">
+
+                    <div style={{height:'100px'}} className="avg-cards2 gr-bg3">
+                    <h3 style={{color:'black'}} className="stats-section-heading2">SS Vitran</h3>
                     <h4>Total SS Vitran Reported : {statsdetails.ssvitrandetails}</h4>
                     </div>
                     </div>
                     </div>
-                    
-                    
                 </div>
+                <div style={{margin:'10px'}} className={`photos-tab-content ${activeTab === 1 ? 'active' : ''}`}>
+                <div className="stats-section-container">
+                  <div>
+                  
+                  <div className="avg-cards2">
+                  <h3 className="stats-section-heading2">Attendance</h3>
+                      <h4>Total Present : {0}</h4>
+                      <h4>Daily Avg    : {0}</h4>
+                      <h4>Weekly Avg    : {0}</h4>
+                      <h4>Monthly Avg    : {0}</h4>
+                      {/* <h4>Morning : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
+                      <h4>Evening : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
+                  </div>
+
+
+                  {/* <Link to="/attendancestats">
+                  <button type="button">More</button>
+                  </Link> */}
+                  </div>
+                  {/* <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                  <div className="avg-cards-daily-avg avg-cards2">
+                      <h2 style={{color:'blue'}}>{(new Date().toLocaleDateString('en-US', { month: 'short', day: 'numeric' }).replace(/(\d+)\s(\w+)/, '$2 $1'))}</h2>
+                      <p style={{fontSize:'12px'}}>Today's Date</p>
+                      
+                  </div>
+                  <div className="avg-cards-weekly-avg avg-cards2">
+                  
+                      <h2 style={{color:'green'}}>{(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'_':'M'}/{(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'_':'E'}</h2>
+                      <p style={{fontSize:'12px'}}>Present</p>
+                      
+                  </div>
+                  <div className="avg-cards-monthly-avg avg-cards2">
+                  
+                      <h2 style={{color:'red'}}>{(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'M':'_'}/{(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'E':'_'}</h2>
+                      <p style={{fontSize:'13px'}}>Absent</p>
+
+                  </div>
+                  </div> */}
+                  </div>
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg2">
+                  <h3 className="stats-section-heading2">Attendance (Selfie)</h3>
+                      <h4>Total Attendance Selfie : {0}</h4>
+                      <h4>Daily Avg              : {0}</h4>
+                      <h4>Weekly Avg             : {0}</h4>
+                      <h4>Monthly Avg            : {0}</h4>
+                      {/* <h4>Morning Selfie : {(statsdetails.morningattendanceselfiedetails)}</h4>
+                      <h4>Evening Selfie : {(statsdetails.eveningattendanceselfiedetails)}</h4> */}
+                  </div>
+                  </div>
+                  {/* <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'blue'}}>5.5</h2>
+                      <p style={{fontSize:'12px'}}>Daily Avg.</p>
+                  </div>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'green'}}>2.15</h2>
+                      <p style={{fontSize:'12px'}}>Weekly Avg.</p>
+                  </div>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'red'}}>1.5</h2>
+                      <p style={{fontSize:'13px'}}>Monthly Avg.</p>
+                  </div>
+                  </div> */}
+                  </div>
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg3">
+                  <h3 style={{color:'black'}} className="stats-section-heading2">Household (Selfie)</h3>
+                      <h4>Total Selfie : {0}</h4>
+                      <h4>Daily Avg              : {0}</h4>
+                      <h4>Weekly Avg             : {0}</h4>
+                      <h4>Monthly Avg            : {0}</h4>
+                  </div>
+                  </div>
+                  {/* <div style={{display:'flex',justifyContent:'space-evenly'}}>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'blue'}}>5.5</h2>
+                      <p style={{fontSize:'12px'}}>Daily Avg.</p>
+                  </div>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'green'}}>2.15</h2>
+                      <p style={{fontSize:'12px'}}>Weekly Avg.</p>
+                  </div>
+                  <div className="avg-cards2">
+                      <h2 style={{color:'red'}}>1.5</h2>
+                      <p style={{fontSize:'13px'}}>Monthly Avg.</p>
+                  </div>
+                  </div> */}
+                  </div>
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg4">
+                  <h3 className="stats-section-heading2">Sansthapak Sadasya</h3>
+                      <h4>Total SS Reported  : {6}</h4>
+                      <h4>Daily Avg              : {0}</h4>
+                      <h4>Weekly Avg             : {0}</h4>
+                      <h4>Monthly Avg            : {0}</h4>
+                      {/* <h4>No of Blocks : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
+                      <h4>No of Villages : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
+                  </div>
+                  </div>
+
+                  </div>
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg1">
+                  <h3 className="stats-section-heading2">Digital Influencer</h3>
+                      <h4>Total Digital Influencers Reported : {0}</h4>
+                      <h4>Daily Avg              : {0}</h4>
+                      <h4>Weekly Avg             : {0}</h4>
+                      <h4>Monthly Avg            : {0}</h4>
+                      {/* <h4>Morning Selfie : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Morning")).length===0?'Absent':'Present'}</h4>
+                      <h4>Evening Selfie : {(attendancedata.filter((ele) => ele.date===(new Date()).toLocaleDateString('en-GB') && ele.period==="Evening")).length===0?'Absent':'Present'}</h4> */}
+                  </div>
+                  </div>
+                  </div>
+                  <div className="stats-section-container">
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg2">
+                  <h3 className="stats-section-heading2">Coaching</h3>
+                  <h4>Total Coaching Reported : {0}</h4>
+                  <h4>Daily Avg              : {0}</h4>
+                  <h4>Weekly Avg             : {0}</h4>
+                  <h4>Monthly Avg            : {0}</h4>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  <div className="stats-section-container">
+                  <div>
+
+                  <div className="avg-cards2 gr-bg3">
+                  <h3 style={{color:'black'}} className="stats-section-heading2">SS Vitran</h3>
+                  <h4>Total SS Vitran Reported : {0}</h4>
+                  <h4>Daily Avg              : {0}</h4>
+                  <h4>Weekly Avg             : {0}</h4>
+                  <h4>Monthly Avg            : {0}</h4>
+                  </div>
+                  </div>
+                  </div>
+              </div>
+              </>
                 )}
             </div>
         </div>  
